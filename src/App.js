17ae30df4865d9e099d0066f31a669edd83c3474
Quang Nguyễn <adquang199x@gmail.com>
2020-11-18 22:59:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Quang Nguyen
+          Nguyễn Xuân Quang
         </p>
         <a
           className="App-link"
