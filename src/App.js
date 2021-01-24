474230e6e08464456c42e89741d5e8e037001651
Quang Nguyễn <adquang199x@gmail.com>
2021-01-24 14:00:02 +0700

    matching routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { Link, Route } from 'react-router-dom';
+import { Link, NavLink, Route, Switch } from 'react-router-dom';
 import TodoFeature from './features/Todo';
 import TodoList from './features/Todo/components/TodoList';
-
+import Loading from './components/Loading/index';
 function App() {
   return (
     <div className="App">
       Header
-      <Link to="/todo">Todo</Link>
-      <Route path="/todo" component={TodoFeature} />
-      <Route path="/todo" component={TodoList} />
+      {/* <Link to="/todo">Todo</Link> */}
+      {/* Navlink có thêm class active */}
+      <NavLink to="/todo" activeClassName="active-menu">Todo</NavLink>
+      <NavLink to="/loading" >Loading</NavLink>
+      {/* Chỉ match với thằng đàu tiên có path được match */}
+      <Switch>
+        {/* 
+          - Mặc định exact = false map với startWith ví dụ /todos match với /todos/123
+          - Nếu để exact thì URL sẽ map chuẩn với path thì route đến trang web mới đúng
+          - dùng exact = true cho routing con
+       */}
+        <Route path="/" component={Loading} exact />
+        <Route path="/loading" component={Loading} />
+        <Route path="/todo" component={TodoList} />
+      </Switch>
       Footer
     </div>
   );
