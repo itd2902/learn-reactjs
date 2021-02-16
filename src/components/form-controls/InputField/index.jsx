69abdfa098907f8e85e613d6040af8c1ce72573a
Quang Nguyễn <adquang199x@gmail.com>
2021-02-16 09:15:48 +0700

    làm form validation với yup

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
+import { Controller } from 'react-hook-form';
 
-InputField.propTypes = {};
+InputField.propTypes = {
+   form: PropTypes.object.isRequired,
+   name: PropTypes.string.isRequired,
+   label: PropTypes.string,
+   disabled: PropTypes.bool,
+};
 
 function InputField(props) {
+   const { form, name, label, disabled } = props;
+   const { errors, formState } = form;
+   const hasError = formState.touched[name] && errors[name];
+
    return (
-      <div>
-         <TextField fullWidth />
-      </div>
+      // Auto binding value by Controller
+      <Controller
+         name={name}
+         label={label}
+         control={form.control}
+         as={TextField}
+         disabled={disabled}
+         fullWidth
+         error={!!hasError}
+         helperText={errors[name]?.message}
+      />
    );
 }
 
