29b4ddf3202e794555ce1c29f6fe3a9e2e578da4
itd2902 <adquang199x@gmail.com>
2021-04-04 14:15:15 +0700

    udpate

--- src/features/Counter/index.jsx
+++ src/features/Counter/index.jsx
@@ -3,10 +3,22 @@ import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrease, increase } from '../Counter/counterSlice';
 import styles from './styles.module.css';
+import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components';
-import { green } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
 CounterFeature.propTypes = {};
 
+const useStyles = makeStyles({
+   root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+   },
+});
 //CSS in JS
 const Title = styled.h1`
    text-align:center,
@@ -24,13 +36,22 @@ function CounterFeature(props) {
       const action = decrease(); //call is action creator
       dispatch(action);
    };
+   const classes = useStyles();
    return (
       <div>
          <Title color="red">HEADING</Title>
          <div className={styles.counter}>Count: {counter}</div>
          <div>
-            <button onClick={handleIncrease}>Increase</button>
-            <button onClick={handleDecrease}>Decrease</button>
+            <Button
+               style={{ marginRight: '10px' }}
+               className={classes.root}
+               onClick={handleIncrease}
+            >
+               Increase
+            </Button>
+            <Button className={classes.root} onClick={handleDecrease}>
+               Decrease
+            </Button>
          </div>
       </div>
    );
